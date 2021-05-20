eb5497c88593e350345c349171924b6e0315dfe2
Apaecida Rosa <lucita.rosa@gmail.com>
2021-05-20 15:55:32 -0300

    Project created and Client Entity

---
+++ backend/src/main/java/com/acrdev/acrcadastro/AcrcadastroApplication.java
@@ -0,0 +1,13 @@
+package com.acrdev.acrcadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrcadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcrcadastroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/acrdev/acrcadastro/AcrcadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrdev.acrcadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrcadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
