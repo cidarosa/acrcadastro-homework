ceb5afaf7856f13cdff1a5adf8a34af30bddfb61
Apaecida Rosa <lucita.rosa@gmail.com>
2021-05-20 18:37:38 -0300

    update ClientService, seeding DB

--- backend/src/main/java/com/acrdev/acrcadastro/services/ClientService.java
+++ backend/src/main/java/com/acrdev/acrcadastro/services/ClientService.java
@@ -4,20 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.acrdev.acrcadastro.entities.Client;
 import com.acrdev.acrcadastro.repositories.ClientRepository;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repository;
-	
-	public List<Client> findAll(){
-		
+
+	@Transactional(readOnly = true)
+	public List<Client> findAll() {
 		return repository.findAll();
-		
 	}
 
 }
