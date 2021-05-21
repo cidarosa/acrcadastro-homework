fed18102dbd34124711612a0e146f317a16c9561
Apaecida Rosa <lucita.rosa@gmail.com>
2021-05-21 16:23:54 -0300

    PUT - método update em ClientResource e ClientService, tratamento exception

--- backend/src/main/java/com/acrdev/acrcadastro/resources/ClientResource.java
+++ backend/src/main/java/com/acrdev/acrcadastro/resources/ClientResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,13 +40,20 @@ public class ClientResource {
 	@PostMapping
 	public ResponseEntity<ClienteDTO> insert(@RequestBody ClienteDTO dto) {
 
-		 dto = service.insert(dto);
-		 
-		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				 .buildAndExpand(dto.getId()).toUri();
-		 
-		 return ResponseEntity.created(uri).body(dto);
+		dto = service.insert(dto);
 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClienteDTO> update(@PathVariable Long id, @RequestBody ClienteDTO dto) {
+
+		dto = service.update(id, dto);
+
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/com/acrdev/acrcadastro/services/ClientService.java
+++ backend/src/main/java/com/acrdev/acrcadastro/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +25,8 @@ public class ClientService {
 	public List<ClienteDTO> findAll() {
 		List<Client> list = repository.findAll();
 		// converter list para listDTO
-		//List<ClienteDTO> listDto = list.stream().map(x -> new ClienteDTO(x)).collect(Collectors.toList());
+		// List<ClienteDTO> listDto = list.stream().map(x -> new
+		// ClienteDTO(x)).collect(Collectors.toList());
 
 		return list.stream().map(x -> new ClienteDTO(x)).collect(Collectors.toList());
 
@@ -33,24 +36,45 @@ public class ClientService {
 	public ClienteDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-		
+
 		return new ClienteDTO(entity);
 	}
 
 	@Transactional
 	public ClienteDTO insert(ClienteDTO dto) {
-		//converter dto para entidade
+		// converter dto para entidade
 		Client entity = new Client();
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
-		
+
 		entity = repository.save(entity);
-		
+
 		return new ClienteDTO(entity);
-		
+
+	}
+
+	@Transactional
+	public ClienteDTO update(Long id, ClienteDTO dto) {
+		// instanciar obj Client - não acessa DB
+
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+
+			entity = repository.save(entity);
+
+			return new ClienteDTO(entity);
+		} catch (EntityNotFoundException e) {
+
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
 	}
 
 }
