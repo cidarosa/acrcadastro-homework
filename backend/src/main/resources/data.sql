6d0e3bf940470151d3bf93265ae765b5962b31ef
Apaecida Rosa <lucita.rosa@gmail.com>
2021-05-22 11:00:52 -0300

    Dados de auditoria, update arquivo data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,12 +1,12 @@
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Jon Snow', '12345678900', 1200, TIMESTAMP WITH TIME ZONE '1986-12-26T10:30:00Z', 0);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Daenerys Targaryen', '78512365498', 5350, TIMESTAMP WITH TIME ZONE '1995-07-20T10:30:00Z', 3);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Jaime Lannister', '36945871265', 20154, TIMESTAMP WITH TIME ZONE '1995-01-01T00:30:00Z', 3);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Cersei Lannister', '87456321457', 8946.36, TIMESTAMP WITH TIME ZONE '1991-09-13T20:50:00Z', 3);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Arya Stark', '28431547893', 4578.01, TIMESTAMP WITH TIME ZONE '1988-03-27T20:50:07Z', 0);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Sansa Stark', '39468751236', 5478, TIMESTAMP WITH TIME ZONE '1986-04-25T19:50:07Z', 1);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Theon Greyjoy', '47813647896', 156987.65, TIMESTAMP WITH TIME ZONE '1987-06-25T12:50:10Z', 2);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Joffrey Baratheon', '13479314587', 6547.36, TIMESTAMP WITH TIME ZONE '1991-08-15T03:30:00Z', 5);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Tyrion Lannister', '54697421365', 20000, TIMESTAMP WITH TIME ZONE '1979-05-08T00:00:50Z', 4);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Brienne de Tarth', '69714532698', 150458, TIMESTAMP WITH TIME ZONE '1976-11-29T14:30:00Z', 3);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES('Ramsay Bolton', '89631456987', 58642.00, TIMESTAMP WITH TIME ZONE '1984-01-01T22:14:50Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Jon Snow', '12345678900', 1200, TIMESTAMP WITH TIME ZONE '1986-12-26T10:30:00Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Daenerys Targaryen', '78512365498', 5350, TIMESTAMP WITH TIME ZONE '1995-07-20T10:30:00Z', 3, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Jaime Lannister', '36945871265', 20154, TIMESTAMP WITH TIME ZONE '1995-01-01T00:30:00Z', 3, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Cersei Lannister', '87456321457', 8946.36, TIMESTAMP WITH TIME ZONE '1991-09-13T20:50:00Z', 3, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Arya Stark', '28431547893', 4578.01, TIMESTAMP WITH TIME ZONE '1988-03-27T20:50:07Z', 0, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Sansa Stark', '39468751236', 5478, TIMESTAMP WITH TIME ZONE '1986-04-25T19:50:07Z', 1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Theon Greyjoy', '47813647896', 156987.65, TIMESTAMP WITH TIME ZONE '1987-06-25T12:50:10Z', 2, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Joffrey Baratheon', '13479314587', 6547.36, TIMESTAMP WITH TIME ZONE '1991-08-15T03:30:00Z', 5, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Tyrion Lannister', '54697421365', 20000, TIMESTAMP WITH TIME ZONE '1979-05-08T00:00:50Z', 4, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Brienne de Tarth', '69714532698', 150458, TIMESTAMP WITH TIME ZONE '1976-11-29T14:30:00Z', 3, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Ramsay Bolton', '89631456987', 58642.00, TIMESTAMP WITH TIME ZONE '1984-01-01T22:14:50Z', 0, NOW());
 
