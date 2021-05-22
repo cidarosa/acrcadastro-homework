74fd4fa122fe347e0bbccdec999e8d9e8881b61e
Apaecida Rosa <lucita.rosa@gmail.com>
2021-05-22 11:31:30 -0300

    Pagination

--- backend/src/main/java/com/acrdev/acrcadastro/services/ClientService.java
+++ backend/src/main/java/com/acrdev/acrcadastro/services/ClientService.java
@@ -1,14 +1,14 @@
 package com.acrdev.acrcadastro.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,14 +25,10 @@ public class ClientService {
 	private ClientRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<ClienteDTO> findAll() {
-		List<Client> list = repository.findAll();
-		// converter list para listDTO
-		// List<ClienteDTO> listDto = list.stream().map(x -> new
-		// ClienteDTO(x)).collect(Collectors.toList());
+	public Page<ClienteDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
 
-		return list.stream().map(x -> new ClienteDTO(x)).collect(Collectors.toList());
-
+		return list.map(x -> new ClienteDTO(x));
 	}
 
 	@Transactional(readOnly = true)
@@ -81,16 +77,16 @@ public class ClientService {
 	}
 
 	public void delete(Long id) {
-		
+
 		try {
 			repository.deleteById(id);
-			
+
 		} catch (EmptyResultDataAccessException e) {
-			
+
 			throw new ResourceNotFoundException("Id not found: " + id);
-			
-		} catch(DataIntegrityViolationException e) {
-			
+
+		} catch (DataIntegrityViolationException e) {
+
 			throw new DatabaseException("Integrity violation");
 		}
 	}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,4 +9,3 @@ INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALU
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Tyrion Lannister', '54697421365', 20000, TIMESTAMP WITH TIME ZONE '1979-05-08T00:00:50Z', 4, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Brienne de Tarth', '69714532698', 150458, TIMESTAMP WITH TIME ZONE '1976-11-29T14:30:00Z', 3, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_At) VALUES('Ramsay Bolton', '89631456987', 58642.00, TIMESTAMP WITH TIME ZONE '1984-01-01T22:14:50Z', 0, NOW());
-
